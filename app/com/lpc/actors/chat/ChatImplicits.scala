8700743d59662a42521680f7fecf0098fc4f4642
vbaklan <uladzimirbaklan@gmail.com>
2020-01-31 15:11:31 -0500

    Remove unexistent implicit

--- app/com/lpc/actors/chat/ChatImplicits.scala
+++ app/com/lpc/actors/chat/ChatImplicits.scala
@@ -7,7 +7,6 @@ object ChatImplicits {
   implicit val inEventFormat: Format[ChatInEvent] = Json.format
   implicit val outEventFormat: Format[ChatOutEvent] = Json.format
   implicit val newMessageEventFormat: Format[ChatNewMessageEvent] = Json.format
-  implicit val sentMessageEventFormat: Format[ChatSentMessageEvent] = Json.format
 
   implicit val messageFlowTransformer: MessageFlowTransformer[ChatInEvent, ChatOutEvent] =
     MessageFlowTransformer.jsonMessageFlowTransformer[ChatInEvent, ChatOutEvent]
